53e3cb64d9f0ee9857f4e562e89d8b74d36593c4
hamilton41 <hamilton032123796@google.com>
2024-05-26 17:45:11 +0800

    s

--- js/object.js
+++ js/object.js
@@ -5,4 +5,18 @@ $(document).ready(function(){
     $(".footer-btn-to-top").on("click", function(){
         $('html, body').animate({ scrollTop: 0 }, 'slow');
     });
+    $(".header-btn-open-icon").on("click", function(){
+        $('.header').addClass("active");
+    });
+    $(".header-btn-close-icon").on("click", function(){
+        $('.header').removeClass("active");
+    });
+});
+
+$(window).on('scroll', function() {
+    if ($(this).scrollTop() > 200) {
+        $('body').addClass('scrolled');
+    } else {
+        $('body').removeClass('scrolled');
+    }
 });
