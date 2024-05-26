09ee151828d412a3a3dfd9fa97310bd2f18411e2
hamilton41 <hamilton032123796@google.com>
2024-05-26 17:02:02 +0800

    s

--- js/object.js
+++ js/object.js
@@ -2,4 +2,7 @@ $(document).ready(function(){
     $(".filter-main-item").on("click", function(){
         $(this).toggleClass("active");
     });
+    $(".footer-btn-to-top").on("click", function(){
+        $('html, body').animate({ scrollTop: 0 }, 'slow');
+    });
 });
